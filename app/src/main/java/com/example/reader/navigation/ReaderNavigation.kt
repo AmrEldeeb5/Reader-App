30fee3effad9988456ce796214f3dcc28a7e283a
Ahmed <ahmedbobos.rashid@gmail.com>
2025-09-12 05:06:57 +0300

    Add onboarding screen components and configuration files

--- app/src/main/java/com/example/reader/navigation/ReaderNavigation.kt
+++ app/src/main/java/com/example/reader/navigation/ReaderNavigation.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.reader.screens.OnBoardingScreen
+import com.example.reader.screens.onboarding.OnBoardingScreen
 import com.example.reader.screens.SignUp.SignUpScreen
 import com.example.reader.screens.home.Home
 import com.example.reader.screens.login.ReaderLoginScreen
